06c0d7341e5300ea17e79aa22d35bd9b8905a0b8
Satyam <satyampd5340@gmail.com>
2021-03-08 19:18:50 +0530

    all bugs fixed

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -4,14 +4,12 @@ import React from 'react';
 const Image = (props) => {
     const {alt_description,urls:{thumb},index,fromModal} = props;
     let modalStyle = '';
-    if (fromModal){
-        modalStyle='modalImg'
-    }
+    
     return <div className='row'>
         <div className='col-md-12 px-0'>
             <div className='rounded-lg overflow-hidden'>
                 
-                <img onClick={()=>props.toggleModal(index)} src={thumb} alt={alt_description} className={`img-fluid ${modalStyle}`}/>
+                <img onClick={()=>props.toggleModal(index)} src={thumb} alt={alt_description} className={'img-fluid'}/>
             </div>
         </div>
     </div>
